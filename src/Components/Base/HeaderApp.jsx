fdc3222bdb92411eb58c47357c110e02978c5d20
MP <1253651@cstj.qc.ca>
2023-12-12 16:42:46 -0500

    ajout-headerApp

---
+++ src/Components/Base/HeaderApp.jsx
@@ -0,0 +1,13 @@
+import Nav from './Nav';
+const HeaderApp = () => {
+
+    return (
+      <header className="headerApp">
+        {/* LOGO ici */}
+
+        <Nav></Nav>
+      </header>
+    )
+  };
+  
+  export default HeaderApp;
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { useState, useRef, useEffect } from "react";
 
+import HeaderApp from "./Base/HeaderApp";
+
 const Layout = () => {
 
   return (
     <div className="layout">
       {/* Les différentes vue de l'app seront affichées dans le main ci-dessous */}
       <main>       
-        {/* <MiniHeader /> */}
+        <HeaderApp />
         <Outlet />
         {/* <Footer /> */}
       </main>
