94e485dcf0499e26b62333ad3823d975fbbac357
MP <1253651@cstj.qc.ca>
2023-12-12 16:42:59 -0500

    creation-nav

---
+++ src/Components/Base/Nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+
+    return (
+      <nav className="navPrinc">
+        <ul>
+            <li className="bio"> Qui est MP ? </li>
+            <li className="projs"> Projets </li>
+            <li className="contact"> Contact </li>
+        </ul>
+      </nav>
+    )
+  };
+  
+  export default Nav;
