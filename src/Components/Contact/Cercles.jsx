d624028d7cf7fea9552ca7691a9b41f9b72b8066
MP <1253651@cstj.qc.ca>
2023-12-13 11:48:07 -0500

    feature-visuel-contact

--- src/Components/Contact/Cercles.jsx
+++ src/Components/Contact/Cercles.jsx
@@ -1,8 +1,6 @@
 const Template = () => {
 
     return (
-      <div className="cercles-container">
-  
       <svg className="cercles" xmlns="http://www.w3.org/2000/svg" width="388.714" height="374.593" viewBox="0 0 388.714 374.593">
       <g id="Rectangle_141" data-name="Rectangle 141" transform="translate(0 4.889)" fill="none" stroke="#000" stroke-width="3">
         <rect width="369.328" height="369.704" rx="184.664" stroke="none"/>
@@ -17,9 +15,6 @@ const Template = () => {
         <rect x="1.5" y="1.5" width="366.704" height="366.704" rx="183.352" fill="none"/>
       </g>
     </svg>
-
-
-      </div>
     )
   };
   
--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -1,12 +1,18 @@
 import './ContactForm.css';
 import Cercles from './Cercles';
+import { HiArrowLongDown } from "react-icons/hi2";
 
 const ContactForm = () => {
-
+  
     return (
       <section className='contactForm-container'>
         <h1 className='contactForm-titre'>Contact</h1>
         <Cercles />
+        <div className="message-merci">
+          <p>Un énorme merci pour l'intérêt que vous portez à travailler avec moi.</p>
+          <p>J'ai hâte de plonger dans des projets passionnants à vos côtés!</p>
+        </div>
+        <p className='form-icone'><HiArrowLongDown size={"70"}/></p>
       </section>
     )
   };
