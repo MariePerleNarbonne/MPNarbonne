d7e67224b659e5a934468485e5314924b4297776
MP <1253651@cstj.qc.ca>
2023-12-13 11:48:30 -0500

    feature-visuel-merci-contact

--- src/Components/Contact/ContactMerci.jsx
+++ src/Components/Contact/ContactMerci.jsx
@@ -1,10 +1,20 @@
 import './ContactMerci.css';
+import { GrLinkedin } from "react-icons/gr";
+import { AiOutlineDribbbleSquare } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 const ContactMerci = () => {
 
     return (
       <section className='contactMerci-container'>
-            
+            <p className='merci-email'>narbonne.mp@gmail.com</p>
+            <div className="merci-icones">
+              <p className='linkedin merci-i'><Link to="https://www.linkedin.com/in/mp-narbonne/"><GrLinkedin size={"40"}/></Link></p>
+              <p className='dribbble merci-i'><Link to="https://dribbble.com/MP-Narbonne"><AiOutlineDribbbleSquare size={"50"}/></Link></p>
+            </div>
+            <div className="merci-signature">
+              <p>MP</p>
+            </div>
       </section>
     )
   };
