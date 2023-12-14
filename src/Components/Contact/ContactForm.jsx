57c5343faaf625badcb56df332420add7b881874
MP <1253651@cstj.qc.ca>
2023-12-14 14:34:45 -0500

    visuel-page-contact-A-NE-PAS-TOUCHER-CA-FONCTIONNE!

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -1,18 +1,26 @@
 import './ContactForm.css';
 import Cercles from './Cercles';
 import { HiArrowLongDown } from "react-icons/hi2";
+import { useState } from 'react';
 
 const ContactForm = () => {
-  
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const updateSize = () => {
+      setWidth(window.innerWidth);
+  };
+
+  window.addEventListener("resize", updateSize);
+
     return (
       <section className='contactForm-container'>
         <h1 className='contactForm-titre'>Contact</h1>
-        <Cercles />
+        {/* <Cercles /> */}
         <div className="message-merci">
           <p>Un énorme merci pour l'intérêt que vous portez à travailler avec moi.</p>
           <p>J'ai hâte de plonger dans des projets passionnants à vos côtés!</p>
         </div>
-        <p className='form-icone'><HiArrowLongDown size={"70"}/></p>
+        <p className='form-icone'><HiArrowLongDown size={width >= 750 ? "100" : "50"}/></p>
       </section>
     )
   };
