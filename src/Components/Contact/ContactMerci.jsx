ae2659befb43547c01346cbed643c5c1c35b3fff
MP <1253651@cstj.qc.ca>
2023-12-17 13:56:49 -0500

    ajout-tooltip-page-contact

--- src/Components/Contact/ContactMerci.jsx
+++ src/Components/Contact/ContactMerci.jsx
@@ -7,37 +7,60 @@ import { Link } from 'react-router-dom';
 
 const ContactMerci = () => {
   const [width, setWidth] = useState(window.innerWidth);
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [tooltipText, setTooltipText] = useState('Cliquer pour copier');
 
   const updateSize = () => {
-      setWidth(window.innerWidth);
+    setWidth(window.innerWidth);
+  };
+
+  const handleEmailClick = () => {
+    const emailText = 'narbonne.mp@gmail.com';
+    const textArea = document.createElement('textarea');
+    textArea.value = emailText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+
+    // Update tooltip text to indicate successful copy
+    setTooltipText('Courriel copié');
+
+    // Show tooltip for a short duration
+    setTooltipVisible(true);
+    setTimeout(() => {
+      setTooltipVisible(false);
+      // Reset tooltip text after hiding
+      setTooltipText('Cliquer pour copier');
+    }, 2000); // Adjust the duration as needed
   };
 
   window.addEventListener("resize", updateSize);
 
-    return (
-      <section className='contactMerci-container'>
-            <div className="merci-infos">
-              <p className='merci-email'>narbonne.mp@gmail.com</p>
-              <div className="merci-icones">
-                <p className='linkedin merci-i'><Link to="https://www.linkedin.com/in/mp-narbonne/"><FaLinkedinIn size={width >= 750 ? "50" : "40"}/></Link></p>
-                <p className='dribbble merci-i'><Link to="https://dribbble.com/MP-Narbonne"><FaDribbble size={width >= 750 ? "70" : "60"}/></Link></p>               
-                <p className='pinterest merci-i'><Link to="https://dribbble.com/MP-Narbonne"><FaPinterest size={width >= 750 ? "70" : "60"}/></Link></p>     
-              </div>
-            </div>
-            <div className="merci-signature">
-              <p>MP</p>
-            </div>
-      </section>
-    )
-    
-  };
-  
-  export default ContactMerci;
+  return (
+    <section className='contactMerci-container'>
+      <div className="merci-infos">
+        <p
+          className='merci-email'
+          onClick={handleEmailClick}
+          onMouseEnter={() => setTooltipVisible(true)}
+          onMouseLeave={() => setTooltipVisible(false)}
+          style={{ cursor: 'pointer' }}
+        >
+          {tooltipVisible && <span className="tooltip-email">{tooltipText}</span>}
+          narbonne.mp@gmail.com
+        </p>
+        <div className="merci-icones">
+          <p className='linkedin merci-i'><Link to="https://www.linkedin.com/in/mp-narbonne/"><FaLinkedinIn size={width >= 750 ? "50" : "40"}/></Link></p>
+          <p className='dribbble merci-i'><Link to="https://dribbble.com/MP-Narbonne"><FaDribbble size={width >= 750 ? "70" : "60"}/></Link></p>               
+          <p className='pinterest merci-i'><Link to="https://dribbble.com/MP-Narbonne"><FaPinterest size={width >= 750 ? "70" : "60"}/></Link></p>     
+        </div>
+      </div>
+      <div className="merci-signature">
+        <p>MP</p>
+      </div>
+    </section>
+  );
+};
 
-// *****************************************
-//             999 999  88888
-//             9  9  9  8   8
-//        ---  9  9  9  88888  ---
-//             9     9  8      
-//             9     9  8
-// *****************************************
+export default ContactMerci;
