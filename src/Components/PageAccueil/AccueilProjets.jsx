4dacfc1a6b511bcfa9e98822bee3a0e924e05fa5
MP <1253651@cstj.qc.ca>
2023-12-18 14:21:53 -0500

    debut-accueilProjets

--- src/Components/PageAccueil/AccueilProjets.jsx
+++ src/Components/PageAccueil/AccueilProjets.jsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import './AccueilProjets.css';
 
 const AccueilProjets = () => {
   return (
@@ -6,9 +7,18 @@ const AccueilProjets = () => {
       <Link className="accueilProjets-btn">Voir les projets </Link>
       <span className="accueilProjets-barres"></span>
       <div className="accueilProjets-imgContainer">
-        <img className="accueilProjets-img accueil-img1" src="" alt="" />
-        <img className="accueilProjets-img accueil-img2" src="" alt="" />
-        <img className="accueilProjets-img accueil-img3" src="" alt="" />
+        <div className="accueilProjets-imgsWrap">
+          <img className="accueilProjets-img accueil-img2" src="/imgs/groovyBeats/Page-Accueil.png" alt="image d'un projet"/>
+            <span className='accueil-img2-span'></span>
+        </div>
+        <div className="accueilProjets-imgsWrap">
+          <img className="accueilProjets-img accueil-img3" src="/imgs/agrotourisme/Page-Accueil.png" alt="image d'un projet"/>
+            <span className='accueil-img3-span'></span>
+        </div>
+        <div className="accueilProjets-imgsWrap">
+          <img className="accueilProjets-img accueil-img1" src="/imgs/bucheron/Destop-Accueil.png" alt="image d'un projet"/>
+            <span className='accueil-img1-span'></span>
+        </div>
       </div>
     </section>
   );
